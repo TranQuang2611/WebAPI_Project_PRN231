c4d1a06d552bf81aa7f318fdedad8534886ca02c
Trần Xuân Quang <quangtx@softdreams.vn>
2023-06-01 15:48:04 +0700

    Add project files.

---
+++ WebAPI_Project_PRN231/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebAPI_Project_PRN231.Models;
+
+namespace WebAPI_Project_PRN231.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
