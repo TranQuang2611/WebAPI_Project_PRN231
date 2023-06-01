459f3173c541a3a1f8e5b65b035a8cd0a6980c32
Trần Xuân Quang <quangtx@softdreams.vn>
2023-06-01 17:05:46 +0700

    add css and template

---
+++ WebAPI_Project_PRN231/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_Project_PRN231.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
