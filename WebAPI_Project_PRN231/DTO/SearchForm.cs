4bce862d0d78733c77e7f73f3ab81adf5f7d78c8
Trần Xuân Quang <quangtx@softdreams.vn>
2023-07-01 11:30:40 +0700

    sửa

--- WebAPI_Project_PRN231/DTO/SearchForm.cs
+++ WebAPI_Project_PRN231/DTO/SearchForm.cs
@@ -2,11 +2,11 @@
 {
     public class SearchForm
     {
-        public List<int?> catId { get; set; } = new List<int?>();
-        public List<int?> colorId { get; set; } = new List<int?>();
-        public List<int?> brandId { get; set; } = new List<int?>();
-        public List<int?> sizeId { get; set; } = new List<int?>();
-        public List<int?> ramId { get; set; } = new List<int?>();
+        public List<int> catId { get; set; } = new List<int>();
+        public List<int> colorId { get; set; } = new List<int>();
+        public List<int> brandId { get; set; } = new List<int>();
+        public List<int> sizeId { get; set; } = new List<int>();
+        public List<int> ramId { get; set; } = new List<int>();
         public string minPrice { get; set; } = string.Empty;
         public string maxPrice { get; set; } = string.Empty;
         public string nameProd { get; set; } = string.Empty;
